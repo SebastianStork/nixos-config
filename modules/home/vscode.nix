7adfa40f4d262e71828b1ec6395b956b9ffd0de6
SebastianStork <sebastian.stork@pm.me>
2024-03-17 13:48:14 +0100

    Declare vscode settings with nix-attribute-set

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -36,25 +36,23 @@
         };
 
         systemd.user.tmpfiles.rules = let
-            settings = builtins.replaceStrings ["\n"] ["\\n"] ''
-                {
-                    "editor.fontFamily": "JetBrainsMono Nerd Font",
-                    "explorer.confirmDelete": false,
-                    "explorer.confirmDragAndDrop": false,
-                    "extensions.autoCheckUpdates": false,
-                    "files.autoSave": "afterDelay",
-                    "git.autofetch": true,
-                    "git.confirmSync": false,
-                    "nix.enableLanguageServer": true,
-                    "nix.serverPath": "nil",
-                    "update.mode": "none",
-                    "git.suggestSmartCommit": false,
-                    "workbench.sideBar.location": "right",
-                    "editor.renderWhitespace": "none",
-                    "workbench.iconTheme": "material-icon-theme",
-                    "editor.minimap.enabled": false
-                }
-            '';
+            settings = builtins.toJSON {
+                "editor.fontFamily" = "JetBrainsMono Nerd Font";
+                "explorer.confirmDelete" = false;
+                "explorer.confirmDragAndDrop" = false;
+                "extensions.autoCheckUpdates" = false;
+                "files.autoSave" = "afterDelay";
+                "git.autofetch" = true;
+                "git.confirmSync" = false;
+                "nix.enableLanguageServer" = true;
+                "nix.serverPath" = "nil";
+                "update.mode" = "none";
+                "git.suggestSmartCommit" = false;
+                "workbench.sideBar.location" = "right";
+                "editor.renderWhitespace" = "none";
+                "workbench.iconTheme" = "material-icon-theme";
+                "editor.minimap.enabled" = false;
+            };
         in [
             "f+ %h/.config/VSCodium/User/settings.json - - - - ${settings}"
             "f+ %h/.config/VSCodium/User/settings-default.json - - - - ${settings}"
