ad3f4782ddfbfd7e3974bda4109d9c889163bde8
SebastianStork <sebastian.stork@pm.me>
2024-03-18 15:17:36 +0100

    Limit number of saved generations to 10

--- modules/system/auto-gc.nix
+++ modules/system/auto-gc.nix
@@ -6,10 +6,12 @@
     options.myConfig.auto-gc.enable = lib.mkEnableOption "";
 
     config = lib.mkIf config.myConfig.auto-gc.enable {
-        nix.gc = {
-            automatic = true;
+        myConfig.nix-helper.enable = true;
+
+        nh.clean = {
+            enable = true;
             dates = "weekly";
-            options = "--delete-older-than 7d";
+            extraArgs = "--keep-since 7d --keep 10";
         };
     };
 }
