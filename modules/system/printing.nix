57f4f7a7dfb290519c427dfed0d5da83dbb36f0b
SebastianStork <sebastian.stork@pm.me>
2024-03-22 22:19:47 +0100

    Rename option to comply with upstream

--- modules/system/printing.nix
+++ modules/system/printing.nix
@@ -9,7 +9,7 @@
         services.printing.enable = true;
         services.avahi = {
             enable = true;
-            nssmdns = true;
+            nssmdns4 = true;
             openFirewall = true;
         };
     };
