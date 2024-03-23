a9507da53a0cfd955fb4849e98ba6efc959b6ec9
SebastianStork <sebastian.stork@pm.me>
2024-03-24 00:10:37 +0100

    Fix comma module

--- modules/system/comma.nix
+++ modules/system/comma.nix
@@ -3,14 +3,16 @@
     config,
     lib,
     ...
-}: {
+}: let
+    cfg = config.myConfig.comma;
+in {
     imports = [inputs.nix-index-database.nixosModules.nix-index];
 
     options.myConfig.comma.enable = lib.mkEnableOption "";
 
-    config = lib.mkIf config.myConfig.comma.enable {
-        programs.command-not-found.enable = false;
-        programs.nix-index.enable = true;
-        programs.nix-index-database.comma.enable = true;
+    config = {
+        programs.command-not-found.enable = !cfg.enable;
+        programs.nix-index.enable = cfg.enable;
+        programs.nix-index-database.comma.enable = cfg.enable;
     };
 }
