37acdefd389f7afbb94a1e2954228802f42f8ea8
SebastianStork <sebastian.stork@pm.me>
2024-03-25 14:59:33 +0100

    Improve contrast between terminal and wallpaper

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -12,7 +12,7 @@
             settings = {
                 font_family = "JetBrainsMono Nerd Font";
                 confirm_os_window_close = 0;
-                background_opacity = "0.7";
+                background_opacity = "0.85";
                 scrollback_lines = 10000;
                 enable_audio_bell = false;
                 update_check_interval = 0;
