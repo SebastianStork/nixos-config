bd72c6860fde1e9cd6722548a41ba4ba493b2d42
SebastianStork <sebastian.stork@pm.me>
2024-04-05 01:13:49 +0200

    Add cachix binary cache

--- hosts/common.nix
+++ hosts/common.nix
@@ -8,6 +8,9 @@
         auto-optimise-store = true;
         warn-dirty = false;
         trusted-users = ["root" "@wheel"];
+
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
 
     time.timeZone = "Europe/Berlin";
