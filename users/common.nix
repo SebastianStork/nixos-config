8d6c69c96c028a6267f1dd3391a00ba32f748c10
SebastianStork <sebastian.stork@pm.me>
2024-04-06 19:40:42 +0200

    Fix inputs in home-manager

--- users/common.nix
+++ users/common.nix
@@ -1,8 +1,11 @@
 {inputs, ...}: {
     imports = [inputs.home-manager.nixosModules.home-manager];
 
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
+    home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        extraSpecialArgs = {inherit inputs;};
+    };
 
     home-manager.sharedModules = [
         ../modules/home
