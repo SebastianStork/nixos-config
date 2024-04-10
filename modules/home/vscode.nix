e35c1942a315d78d8ed140eecd910f3fc9e5fa86
SebastianStork <sebastian.stork@pm.me>
2024-04-10 20:28:24 +0200

    Remove the vscode direnv extension

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -17,7 +17,6 @@
             extensions = [
                 pkgs.vscode-extensions.jnoortheen.nix-ide
                 pkgs.vscode-extensions.pkief.material-icon-theme
-                pkgs.vscode-extensions.mkhl.direnv
             ];
         };
 
