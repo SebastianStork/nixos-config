83ea93fa5c7b4fb82e781f7647fe383ab4a5d1ce
SebastianStork <sebastian.stork@pm.me>
2024-04-14 00:09:26 +0200

    Enable a basic waybar setup

--- modules/home/de/hyprland.nix
+++ modules/home/de/hyprland.nix
@@ -53,6 +53,46 @@ in {
             ];
         };
 
+        programs.waybar = {
+            enable = true;
+            systemd.enable = true;
+
+            settings = {
+                mainBar = {
+                    layer = "top";
+                    position = "top";
+                    spacing = 3;
+
+                    modules-left = ["clock"];
+                    modules-center = ["hyprland/workspaces"];
+                    modules-right = ["tray" "wireplumber" "backlight" "battery"];
+
+                    "hyprland/workspaces" = {
+                        active-only = false;
+                        all-outputs = true;
+                    };
+
+                    backlight = {
+                        device = "amdgpu_bl1";
+                    };
+                };
+            };
+
+            style = ''
+                * {
+                    border: none;
+                    border-radius: 0px;
+                    font-family: "JetBrainsMono Nerd Font";
+                    font-size: 14px;
+                }
+
+                window#waybar {
+                    background-color: rgba(43, 48, 59, 0.5);
+                    color: #ffffff;
+                }
+            '';
+        };
+
         myConfig.rofi.enable = true;
         services.cliphist.enable = true;
 
