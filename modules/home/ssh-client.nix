903484a4cdd84f02e460770f2b879cc8619cb826
SebastianStork <sebastian.stork@pm.me>
2024-04-30 12:27:46 +0200

    Remove kluebero ssh connection

--- modules/home/ssh-client.nix
+++ modules/home/ssh-client.nix
@@ -8,13 +8,6 @@
     config = lib.mkIf config.myConfig.ssh-client.enable {
         programs.ssh = {
             enable = true;
-
-            matchBlocks.klue-server1 = {
-                hostname = "10.5.251.175";
-                user = "seb";
-                identitiesOnly = true;
-                identityFile = ["~/.ssh/kluebero/id_ed25519"];
-            };
         };
 
         services.ssh-agent.enable = true;
