8730e3a2eeef63cc1a6ab47d2ee2328c487e0bf1
SebastianStork <sebastian.stork@pm.me>
2024-05-02 16:16:55 +0200

    Improve clarity of ls alias generation

--- modules/home/shell/enhancement.nix
+++ modules/home/shell/enhancement.nix
@@ -18,22 +18,22 @@
             lsAliases = let
                 list = "${lib.getExe pkgs.eza} --header --group --time-style=long-iso --group-directories-first --sort=name --icons=auto --git --git-repos-no-status --binary";
 
-                getFlag = alias:
+                convertFlagAliasToFlag = char:
                     {
                         a = "--all";
                         d = "--only-dirs";
                         f = "--only-files";
                     }
-                    .${alias};
-                convertAliasesToFlags = str: "${lib.concatStringsSep " " (lib.forEach (lib.stringToCharacters str) getFlag)}";
-                flagCombos = lib.crossLists (a: b: "${a}${b}") [["" "a"] ["" "d" "f"]];
-
-                flaggedAliases = lib.concatMapAttrs (name: value: {
+                    .${char};
+                convertFlagAliasesToFlags = str: "${lib.concatStringsSep " " (lib.forEach (lib.stringToCharacters str) convertFlagAliasToFlag)}";
+                flagAliasCombos = lib.crossLists (a: b: "${a}${b}") [["" "a"] ["" "d" "f"]];
+                flagAttrs = lib.genAttrs flagAliasCombos convertFlagAliasesToFlags;
+                aliasTemplate = name: value: {
                     "l${name}" = "${list} --oneline --dereference ${value}";
                     "ll${name}" = "${list} --long ${value}";
                     "lt${name}" = "${list} --tree ${value}";
-                })
-                (lib.genAttrs flagCombos convertAliasesToFlags);
+                };
+                flaggedAliases = lib.concatMapAttrs aliasTemplate flagAttrs;
             in
                 flaggedAliases // {ls = "l";};
 
