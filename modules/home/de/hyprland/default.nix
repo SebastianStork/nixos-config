25c17ea10256371f93dce6163d2f54a3618a3c8a
SebastianStork <sebastian.stork@pm.me>
2024-05-28 08:24:40 +0200

    Disable phantom monitor

--- modules/home/de/hyprland/default.nix
+++ modules/home/de/hyprland/default.nix
@@ -38,6 +38,7 @@
         ];
 
         monitor = [
+          "Unknown-1,disable"
           "DP-1,2560x1440@144,0x0,1"
           "eDP-1,1920x1080@60,0x0,1"
           ",preferred,auto,1,mirror,DP-1"
