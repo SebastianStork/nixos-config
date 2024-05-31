314029a656b287865fedb5b09a3ad10af5fc2324
SebastianStork <sebastian.stork@pm.me>
2024-05-31 17:41:52 +0200

    Reduce number of rofi config files

--- wrappers/rofi/default.nix
+++ wrappers/rofi/default.nix
@@ -1,6 +1,6 @@
 { assembleWrapper, moduleArgs, ... }:
 let
-  inherit (moduleArgs) pkgs;
+  inherit (moduleArgs) pkgs lib;
 in
 {
   theme ? "dark",
@@ -10,16 +10,36 @@ assembleWrapper {
 
   flags =
     let
-      color-file =
+      theming =
         {
-          dark = ./dark.rasi;
-          light = ./light.rasi;
+          dark = ''
+            * {
+              background:     #1E2127FF;
+              background-alt: #282B31FF;
+              foreground:     #FFFFFFFF;
+              selected:       #61AFEFFF;
+              active:         #98C379FF;
+              urgent:         #E06C75FF;
+            }
+          '';
+          light = ''
+            * {
+              background:     #F1F1F1FF;
+              background-alt: #E0E0E0FF;
+              foreground:     #252525FF;
+              selected:       #008EC4FF;
+              active:         #10A778FF;
+              urgent:         #C30771FF;
+            }
+          '';
         }
         .${theme};
-      rofi-config = pkgs.concatText "rofi-config" [
-        ./config.rasi
-        color-file
-      ];
+      rofi-config = pkgs.writeText "rofi-config" (
+        lib.concatLines [
+          theming
+          (builtins.readFile ./config.rasi)
+        ]
+      );
     in
     [
       "-config"
