bed78d34ca86332f95c0aebaefb69e87071b15b7
SebastianStork <sebastian.stork@pm.me>
2024-06-26 23:19:10 +0200

    Remove code repitition

--- modules/home/theme.nix
+++ modules/home/theme.nix
@@ -11,70 +11,41 @@ in
   options.myConfig.theme = lib.mkOption { type = lib.types.str; };
 
   config = lib.mkMerge [
-    (lib.mkIf (cfg.theme == "dark") {
-      dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-
+    (lib.mkIf ((cfg.theme == "dark") || (cfg.theme == "light")) {
       gtk = {
         enable = true;
-
         gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-
-        theme.name = "Adwaita-dark";
         theme.package = pkgs.gnome.gnome-themes-extra;
-
-        iconTheme.name = "Papirus-Dark";
         iconTheme.package = pkgs.papirus-icon-theme;
-
         font.name = "Open Sans";
         font.package = pkgs.open-sans;
       };
-
       qt = {
         enable = true;
-        platformTheme.name = "adwaita";
-        style.name = "adwaita-dark";
         style.package = pkgs.adwaita-qt;
+        platformTheme.name = "adwaita";
       };
-
       home.pointerCursor = {
-        name = "Bibata-Original-Classic";
+        gtk.enable = true;
         package = pkgs.bibata-cursors;
         size = 24;
-        gtk.enable = true;
       };
+    })
+
+    (lib.mkIf (cfg.theme == "dark") {
+      dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      gtk.theme.name = "Adwaita-dark";
+      gtk.iconTheme.name = "Papirus-Dark";
+      qt.style.name = "adwaita-dark";
+      home.pointerCursor.name = "Bibata-Original-Classic";
     })
 
     (lib.mkIf (cfg.theme == "light") {
       dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-light";
-
-      gtk = {
-        enable = true;
-
-        gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-
-        theme.name = "Adwaita";
-        theme.package = pkgs.gnome.gnome-themes-extra;
-
-        iconTheme.name = "Papirus";
-        iconTheme.package = pkgs.papirus-icon-theme;
-
-        font.name = "Open Sans";
-        font.package = pkgs.open-sans;
-      };
-
-      qt = {
-        enable = true;
-        platformTheme.name = "adwaita";
-        style.name = "adwaita";
-        style.package = pkgs.adwaita-qt;
-      };
-
-      home.pointerCursor = {
-        name = "Bibata-Original-Ice";
-        package = pkgs.bibata-cursors;
-        size = 24;
-        gtk.enable = true;
-      };
+      gtk.theme.name = "Adwaita";
+      gtk.iconTheme.name = "Papirus";
+      qt.style.name = "adwaita";
+      home.pointerCursor.name = "Bibata-Original-Ice";
     })
   ];
 }
