b3a5fa3345e1e04d8b0a0ddca197a97f993c0a6a
SebastianStork <sebastian.stork@pm.me>
2024-07-04 23:55:51 +0200

    Fix ctrl + arrow behavior in zsh

--- modules/home-manager/shell/default.nix
+++ modules/home-manager/shell/default.nix
@@ -38,6 +38,9 @@
         zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}
         zstyle ':completion:*' menu no
         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls $realpath'
+
+        bindkey "^[[1;5D" backward-word
+        bindkey "^[[1;5C" forward-word
       '';
     };
 
