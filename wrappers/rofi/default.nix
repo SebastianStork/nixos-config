8b69c15eae42946e09b73d65c8ba8309b1cb684c
SebastianStork <sebastian.stork@pm.me>
2024-07-17 00:06:13 +0200

    Extract rofi theming into separate files

--- wrappers/rofi/default.nix
+++ wrappers/rofi/default.nix
@@ -17,38 +17,17 @@
           let
             theming =
               {
-                dark = ''
-                  * {
-                    background:     #1E2127FF;
-                    background-alt: #282B31FF;
-                    foreground:     #FFFFFFFF;
-                    selected:       #61AFEFFF;
-                    active:         #98C379FF;
-                    urgent:         #E06C75FF;
-                  }
-                '';
-                light = ''
-                  * {
-                    background:     #F1F1F1FF;
-                    background-alt: #E0E0E0FF;
-                    foreground:     #252525FF;
-                    selected:       #008EC4FF;
-                    active:         #10A778FF;
-                    urgent:         #C30771FF;
-                  }
-                '';
+                dark = ./dark-theme.rasi;
+                light = ./light-theme.rasi;
               }
               .${theme};
-            rofi-config = pkgs.writeText "rofi-config" (
-              lib.concatLines [
-                theming
-                (builtins.readFile ./config.rasi)
-              ]
-            );
           in
           [
             "-config"
-            rofi-config
+            (pkgs.concatText "rofi-config" [
+              ./config.rasi
+              theming
+            ])
           ];
       };
     }
