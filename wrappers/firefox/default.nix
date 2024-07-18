0f5efb1628f7325a7de01aa583c9190833e1acbf
SebastianStork <sebastian.stork@pm.me>
2024-07-18 13:44:39 +0200

    Fix hardcoded system architecture

--- wrappers/firefox/default.nix
+++ wrappers/firefox/default.nix
@@ -10,7 +10,7 @@
     {
       wrappers.firefox = {
         basePackage = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-          extraPolicies.ExtensionSettings = import ./extensions.nix { inherit inputs lib; };
+          extraPolicies.ExtensionSettings = import ./extensions.nix { inherit inputs pkgs lib; };
           extraPrefs = import ./preferences.nix { inherit inputs; };
         };
       };
--- wrappers/firefox/extensions.nix
+++ wrappers/firefox/extensions.nix
@@ -1,10 +1,10 @@
-{ inputs, lib, ... }:
+{ inputs, pkgs, lib, ... }:
 let
   extension = shortId: uuid: {
     name = uuid;
     value = {
       install_url = "file:///${
-        inputs.firefox-addons.packages.x86_64-linux.${shortId}
+        inputs.firefox-addons.packages.${pkgs.system}.${shortId}
       }/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/${uuid}.xpi";
       installation_mode = "force_installed";
       default_area = "menupanel";
