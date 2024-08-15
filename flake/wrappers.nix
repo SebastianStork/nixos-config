87784510f1ec986dee37e76dc3b6130cfdda4db2
SebastianStork <sebastian.stork@pm.me>
2024-08-15 16:14:48 +0200

    Refactor wrapper module args

--- flake/wrappers.nix
+++ flake/wrappers.nix
@@ -8,8 +8,11 @@
       ...
     }:
     {
-      _module.args.wrappers = lib.concatMapAttrs (name: _: {
-        ${lib.removeSuffix ".nix" name} = import "${self}/wrappers/${name}" { inherit inputs pkgs lib; };
-      }) (builtins.readDir "${self}/wrappers");
+      _module.args.wrappers = lib.mapAttrs' (
+        name: _:
+        lib.nameValuePair (lib.removeSuffix ".nix" name) (
+          import "${self}/wrappers/${name}" { inherit inputs pkgs lib; }
+        )
+      ) (builtins.readDir "${self}/wrappers");
     };
 }
