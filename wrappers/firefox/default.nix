3f4d214a802de00423476a74e8ba1262d9368137
SebastianStork <sebastian.stork@pm.me>
2024-08-26 22:42:29 +0200

    Update firefox

--- wrappers/firefox/default.nix
+++ wrappers/firefox/default.nix
@@ -9,7 +9,7 @@
   modules = [
     {
       wrappers.firefox = {
-        basePackage = pkgs.wrapFirefox pkgs.firefox-esr-128-unwrapped {
+        basePackage = pkgs.wrapFirefox pkgs.firefox-unwrapped {
           extraPolicies.ExtensionSettings = import ./extensions.nix { inherit inputs pkgs lib; };
           extraPrefs = import ./preferences.nix { inherit inputs; };
         };
