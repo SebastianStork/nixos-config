92147e220bca48d4612ac4b44e72d33f8050f862
SebastianStork <sebastian.stork@pm.me>
2024-08-26 02:37:06 +0200

    Set nextcloud maintenance time

--- modules/system/nextcloud/default.nix
+++ modules/system/nextcloud/default.nix
@@ -19,8 +19,10 @@
       home = "/data/nextcloud";
       hostName = config.networking.fqdn;
 
-      autoUpdateApps.enable = true;
-      extraAppsEnable = true;
+      autoUpdateApps = {
+        enable = true;
+        startAt = "04:00:00";
+      };
       extraApps = {
         inherit (config.services.nextcloud.package.packages.apps) contacts calendar;
       };
@@ -35,6 +37,7 @@
       settings = {
         log_type = "file";
         default_phone_region = "DE";
+        maintenance_window_start = "2"; # UTC
       };
     };
   };
