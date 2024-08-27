357a33b81fdf50af321948b2f04bac55fd75e5ca
SebastianStork <sebastian.stork@pm.me>
2024-08-27 18:13:04 +0200

    Fix repeating "service" key

--- modules/system/nextcloud/default.nix
+++ modules/system/nextcloud/default.nix
@@ -13,56 +13,58 @@
       group = config.services.nextcloud.config.dbuser;
     };
 
-    services.nextcloud = {
-      enable = true;
-      package = pkgs.nextcloud29;
-      home = "/data/nextcloud";
-      hostName = config.networking.fqdn;
-      configureRedis = true;
-
-      database.createLocally = true;
-      config = {
-        dbtype = "pgsql";
-        adminuser = "admin";
-        adminpassFile = config.sops.secrets."nextcloud/admin-pass".path;
-      };
-
-      https = true;
-      settings = {
-        overwriteProtocol = "https";
-        trusted_proxies = [ "127.0.0.1" ];
-        log_type = "file";
-        default_phone_region = "DE";
-        maintenance_window_start = "2"; # UTC
-      };
-
-      phpOptions."opcache.interned_strings_buffer" = "16";
-
-      autoUpdateApps = {
+    services = {
+      nextcloud = {
         enable = true;
-        startAt = "04:00:00";
-      };
-      extraApps = {
-        inherit (config.services.nextcloud.package.packages.apps) contacts calendar;
-      };
-    };
+        package = pkgs.nextcloud29;
+        home = "/data/nextcloud";
+        hostName = config.networking.fqdn;
+        configureRedis = true;
 
-    services.nginx = {
-      enable = true;
-      virtualHosts.${config.services.nextcloud.hostName}.listen = [
-        {
-          addr = "0.0.0.0";
-          port = 8080;
-        }
-      ];
-    };
+        database.createLocally = true;
+        config = {
+          dbtype = "pgsql";
+          adminuser = "admin";
+          adminpassFile = config.sops.secrets."nextcloud/admin-pass".path;
+        };
 
-    services.tailscale.permitCertUid = "caddy";
-    services.caddy = {
-      enable = true;
-      virtualHosts.${config.services.nextcloud.hostName}.extraConfig = ''
-        reverse_proxy localhost:8080
-      '';
+        https = true;
+        settings = {
+          overwriteProtocol = "https";
+          trusted_proxies = [ "127.0.0.1" ];
+          log_type = "file";
+          default_phone_region = "DE";
+          maintenance_window_start = "2"; # UTC
+        };
+
+        phpOptions."opcache.interned_strings_buffer" = "16";
+
+        autoUpdateApps = {
+          enable = true;
+          startAt = "04:00:00";
+        };
+        extraApps = {
+          inherit (config.services.nextcloud.package.packages.apps) contacts calendar;
+        };
+      };
+
+      nginx = {
+        enable = true;
+        virtualHosts.${config.services.nextcloud.hostName}.listen = [
+          {
+            addr = "0.0.0.0";
+            port = 8080;
+          }
+        ];
+      };
+
+      tailscale.permitCertUid = "caddy";
+      caddy = {
+        enable = true;
+        virtualHosts.${config.services.nextcloud.hostName}.extraConfig = ''
+          reverse_proxy localhost:8080
+        '';
+      };
     };
   };
 }
