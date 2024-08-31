97af022c1af01ea7de3d136870bf30773dce8771
SebastianStork <sebastian.stork@pm.me>
2024-08-31 22:56:42 +0200

    Update

--- hosts/north/nvidia.nix
+++ hosts/north/nvidia.nix
@@ -7,6 +7,7 @@
 {
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
+    open = true;
     modesetting.enable = true;
     powerManagement.enable = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
