e0606824027e61c667d9b9e736d8cbfe4f5e73a6
SebastianStork <sebastian.stork@pm.me>
2024-10-21 12:40:06 +0200

    Install quickemu

--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,10 +1,17 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   options.myConfig.virtualisation.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.myConfig.virtualisation.enable {
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    environment.systemPackages = [ pkgs.quickemu ];
 
     virtualisation.virtualbox.host.enable = true;
   };
