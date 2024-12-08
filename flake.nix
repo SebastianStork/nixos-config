3d96239b5d40bd900a089e587df41b4f8a49aa78
SebastianStork <sebastian.stork@pm.me>
2024-12-08 01:00:52 +0100

    Switch channel from unstable to 24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
@@ -12,7 +12,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
