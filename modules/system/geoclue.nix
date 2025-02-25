a2bd7dd92f0e2a78b590574c6de25dd81ba1143e
SebastianStork <sebastian.stork@pm.me>
2025-02-25 23:06:21 +0100

    Switch geoprovider from google to beacondb

--- modules/system/geoclue.nix
+++ modules/system/geoclue.nix
@@ -3,23 +3,10 @@
   options.myConfig.geoclue.enable = lib.mkEnableOption "";
 
   config = lib.mkIf config.myConfig.geoclue.enable {
-    sops = {
-      secrets."geolocation-api-key" = { };
-
-      templates."geoclue-location-service.conf" = {
-        owner = "geoclue";
-        path = "/etc/geoclue/conf.d/location-service.conf";
-        content = ''
-          [wifi]
-          url=https://www.googleapis.com/geolocation/v1/geolocate?key=${
-            config.sops.placeholder."geolocation-api-key"
-          }
-        '';
-      };
-    };
-
     services.geoclue2 = {
       enable = true;
+      geoProviderUrl = "https://beacondb.net/v1/geolocate";
+      
       appConfig.gammastep = {
         isAllowed = true;
         isSystem = false;
