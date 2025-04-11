005c01fe21b93a15e5d2d22c038d7c4bb290cae7
SebastianStork <sebastian.stork@pm.me>
2025-04-11 12:36:17 +0200

    Update nextcloud to version 31

--- modules/system/nextcloud/default.nix
+++ modules/system/nextcloud/default.nix
@@ -40,7 +40,7 @@ in
 
     services.nextcloud = {
       enable = true;
-      package = pkgs.nextcloud30;
+      package = pkgs.nextcloud31;
       hostName = "${cfg.subdomain}.${config.networking.domain}";
 
       database.createLocally = true;
