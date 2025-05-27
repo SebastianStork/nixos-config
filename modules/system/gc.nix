23ac618fb67369d17ca8ebce183ad24770eeacd6
SebastianStork <sebastian.stork@pm.me>
2025-05-27 21:23:07 +0200

    Garbage collect weekly instead of daily

--- modules/system/gc.nix
+++ modules/system/gc.nix
@@ -7,7 +7,7 @@
       enable = true;
       clean = {
         enable = true;
-        dates = "daily";
+        dates = "weekly";
         extraArgs = "--keep 10 --keep-since 7d";
       };
     };
