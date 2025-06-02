bf6c0040d9662bddea2c038c339cf7b15656ce91
SebastianStork <sebastian.stork@pm.me>
2025-06-02 23:23:54 +0200

    Update forgejo mirrors every 1h

--- modules/system/services/forgejo/default.nix
+++ modules/system/services/forgejo/default.nix
@@ -34,6 +34,7 @@ in
         };
         service.DISABLE_REGISTRATION = true;
         session.PROVIDER = "db";
+        mirror.DEFAULT_INTERVAL = "1h";
 
         # https://forgejo.org/docs/latest/admin/recommendations
         database.SQLITE_JOURNAL_MODE = "WAL";
