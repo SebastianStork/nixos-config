93f30557868a01c1a5527282062d236b12b743e0
SebastianStork <sebastian.stork@pm.me>
2025-06-08 01:44:05 +0200

    Enable uptime-kuma on cumulus

--- hosts/cumulus/default.nix
+++ hosts/cumulus/default.nix
@@ -1,13 +1,25 @@
-_: {
+{ config, ... }:
+{
   system.stateVersion = "24.11";
 
   custom = {
     sops.enable = true;
     boot.loader.grub.enable = true;
 
-    services.tailscale = {
-      enable = true;
-      ssh.enable = true;
+    services = {
+      tailscale = {
+        enable = true;
+        ssh.enable = true;
+      };
+
+      uptimeKuma = {
+        enable = true;
+        domain = "uptime.${config.custom.services.tailscale.domain}";
+      };
+
+      caddy.virtualHosts.uptimeKuma = {
+        inherit (config.custom.services.uptimeKuma) domain port;
+      };
     };
   };
 }
