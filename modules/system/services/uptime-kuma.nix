7cd535e424cdc27f63dd56ccf509cc6517c6876a
SebastianStork <sebastian.stork@pm.me>
2025-06-08 01:43:48 +0200

    Add uptime-kuma module

---
+++ modules/system/services/uptime-kuma.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.custom.services.uptimeKuma;
+in
+{
+  options.custom.services.uptimeKuma = {
+    enable = lib.mkEnableOption "";
+    domain = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = "";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 3001;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.uptime-kuma = {
+      enable = true;
+      settings.PORT = toString cfg.port;
+    };
+  };
+}
