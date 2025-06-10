da7307f259d936f342155d85b8fec87171b560b2
SebastianStork <sebastian.stork@pm.me>
2025-06-10 13:13:31 +0200

    Use latest kernel again

--- hosts/north/default.nix
+++ hosts/north/default.nix
@@ -1,18 +1,7 @@
 { pkgs, ... }:
 {
   system.stateVersion = "23.11";
-  boot.kernelPackages = pkgs.linuxPackagesFor (
-    pkgs.linux_6_14.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "sha256-IYF/GZjiIw+B9+T2Bfpv3LBA4U+ifZnCfdsWznSXl6k=";
-        };
-        version = "6.14.6";
-        modDirVersion = "6.14.6";
-      };
-    }
-  );
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   custom = {
     sops.enable = true;
