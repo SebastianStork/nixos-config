3d4c301bd8d98aea9df630ee59fc35fe84baac50
SebastianStork <sebastian.stork@pm.me>
2025-06-13 20:56:29 +0200

    Set forgejo ssh port with an option

--- modules/system/services/forgejo/ssh.nix
+++ modules/system/services/forgejo/ssh.nix
@@ -1,10 +1,22 @@
 { config, lib, ... }:
+let
+  cfg = config.custom.services.forgejo.ssh;
+in
 {
-  options.custom.services.forgejo.ssh.enable = lib.mkEnableOption "";
+  options.custom.services.forgejo.ssh = {
+    enable = lib.mkEnableOption "";
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 22;
+    };
+  };
 
-  config = lib.mkIf config.custom.services.forgejo.ssh.enable {
+  config = lib.mkIf cfg.enable {
+    services.forgejo.settings.server.SSH_PORT = cfg.port;
+
     services.openssh = {
       enable = true;
+      ports = lib.mkForce [ cfg.port ];
       authorizedKeysFiles = lib.mkForce [ "${config.services.forgejo.stateDir}/.ssh/authorized_keys" ];
       settings = {
         PasswordAuthentication = false;
