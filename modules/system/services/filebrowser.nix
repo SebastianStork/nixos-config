e1066c208347338a3e277d866440b02f44d6e8ee
SebastianStork <sebastian.stork@pm.me>
2025-09-02 16:32:28 +0200

    filebrowser: Init module

---
+++ modules/system/services/filebrowser.nix
@@ -0,0 +1,39 @@
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
+let
+  cfg = config.custom.services.filebrowser;
+in
+{
+  imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/filebrowser.nix" ];
+
+  options.custom.services.filebrowser = {
+    enable = lib.mkEnableOption "";
+    domain = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = "";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 44093;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    meta = {
+      domains.list = [ cfg.domain ];
+      ports.tcp.list = [ cfg.port ];
+    };
+
+    services.filebrowser = {
+      enable = true;
+      settings = {
+        inherit (cfg) port;
+        noauth = true;
+      };
+    };
+  };
+}
