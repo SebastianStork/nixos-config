331ba44b751e0151a4bd056fabb3ab2a35323db0
SebastianStork <sebastian.stork@pm.me>
2025-09-08 16:24:15 +0200

    sops: Use persisted key path when using impermanence

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -31,7 +31,9 @@ in
 
   config = lib.mkIf cfg.enable {
     sops = {
-      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      age.sshKeyPaths = [
+        "${lib.optionalString config.custom.impermanence.enable "/persist"}/etc/ssh/ssh_host_ed25519_key"
+      ];
       defaultSopsFile = absoluteSecretsPath;
     };
   };
