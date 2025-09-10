74c584c1c4484ec726a76340da7d7825e3f68f5c
SebastianStork <sebastian.stork@pm.me>
2025-09-10 13:25:18 +0200

    Uninstall unneeded packages

--- users/seb/@desktop/home.nix
+++ users/seb/@desktop/home.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+_: {
   imports = [ ../home.nix ];
 
   home.stateVersion = "23.11";
@@ -8,13 +7,6 @@
     sops.agePublicKey = "age1p32cyzakxtcx346ej82ftln4r2aw2pcuazq3583s85nzsan4ygqsj32hjf";
     theme = "dark";
   };
-
-  home.packages = [
-    pkgs.ffmpeg
-    pkgs.obs-studio
-    pkgs.davinci-resolve
-    pkgs.gimp
-  ];
 
   wayland.windowManager.hyprland.settings.monitor = [ "DP-1,2560x1440@144,0x0,1" ];
 }
