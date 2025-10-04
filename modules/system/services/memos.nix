f1c0a77ce0aa8fdcc5ed9d243b4049bb27f9be52
SebastianStork <sebastian.stork@pm.me>
2025-10-04 15:17:00 +0200

    memos: Add 25.11 todo

--- modules/system/services/memos.nix
+++ modules/system/services/memos.nix
@@ -26,7 +26,9 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    warnings = lib.optional (lib.pathExists "${modulesPath}/services/misc/memos.nix") "TODO: Use memos module from stable nixpkgs";
+    warnings =
+      lib.optional (lib.pathExists "${modulesPath}/services/misc/memos.nix") "TODO: Use memos module from stable nixpkgs"
+      ++ lib.optional (lib.versionAtLeast lib.version "25.11") "TODO: Use memos package from stable nixpkgs";
 
     meta = {
       domains.list = [ cfg.domain ];
