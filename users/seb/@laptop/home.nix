ceeac97675912e5e4373e2009329cb913b48f69b
SebastianStork <git@sstork.dev>
2026-01-14 14:16:46 +0100

    laptop: Disable vrr

--- users/seb/@laptop/home.nix
+++ users/seb/@laptop/home.nix
@@ -9,7 +9,7 @@ _: {
   };
 
   wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-1,2880x1920@120,0x0,2,vrr,2"
+    "eDP-1,2880x1920@120,0x0,2"
     ",preferred,auto,1,mirror,eDP-1"
   ];
 }
