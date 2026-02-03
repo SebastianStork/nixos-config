59551461e646c6f21b9a4b9093321145940ab7a5
SebastianStork <git@sstork.dev>
2026-02-04 00:17:14 +0100

    lib: Refactor

--- flake-parts/lib.nix
+++ flake-parts/lib.nix
@@ -1,7 +1,19 @@
-{ self, inputs, ... }:
+{ self, lib, ... }:
 {
-  flake.lib = import "${self}/lib" {
-    inherit (inputs.nixpkgs) lib;
-    inherit self;
+  flake.lib = {
+    isPrivateDomain = domain: domain |> lib.hasSuffix ".splitleaf.de";
+
+    listNixFilesRecursively =
+      dir: dir |> lib.filesystem.listFilesRecursive |> lib.filter (lib.hasSuffix ".nix");
+
+    listDirectoryNames =
+      path: path |> builtins.readDir |> lib.filterAttrs (_: type: type == "directory") |> lib.attrNames;
+
+    genAttrs = f: names: lib.genAttrs names f;
+
+    mkUnprotectedMessage =
+      name: "${name} should only be exposed on private networks; access control isn't yet configured";
+
+    relativePath = path: path |> toString |> lib.removePrefix "${self}/";
   };
 }
