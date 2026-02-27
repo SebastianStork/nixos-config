da5df4f2ee8d84de31beeff69ed1ac03e7fa1a58
SebastianStork <git@sstork.dev>
2026-02-27 18:39:20 +0100

    nas: Disable nebula lighthouse

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -8,14 +8,10 @@
     boot.loader.grub.enable = true;
 
     networking = {
-      overlay = {
-        address = "10.254.250.6";
-        isLighthouse = true;
-      };
+      overlay.address = "10.254.250.6";
       underlay = {
         interface = "enp2s0";
         cidr = "192.168.0.64/24";
-        isPublic = true;
         gateway = "192.168.0.1";
       };
     };
