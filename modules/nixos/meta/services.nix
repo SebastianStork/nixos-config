b26751a5fbef67d47f36d6c1b17907b8161c61e4
SebastianStork <git@sstork.dev>
2026-03-10 00:01:36 +0100

    meta/services: Init module

---
+++ modules/nixos/meta/services.nix
@@ -0,0 +1,27 @@
+{ lib, ... }:
+{
+  options.custom.meta.services = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule (
+        { name, ... }:
+        {
+          options = {
+            title = lib.mkOption {
+              type = lib.types.nonEmptyStr;
+              default = name;
+            };
+            url = lib.mkOption {
+              type = lib.types.nonEmptyStr;
+              default = "https://${name}";
+            };
+            icon = lib.mkOption {
+              type = lib.types.nonEmptyStr;
+              default = "";
+            };
+          };
+        }
+      )
+    );
+    default = { };
+  };
+}
