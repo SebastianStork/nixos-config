9f5876512715bbb098961ea740f51a66e978cd88
SebastianStork <git@sstork.dev>
2026-03-12 19:54:10 +0100

    firefox: Install extension to override new tab behavior

--- modules/home/programs/firefox.nix
+++ modules/home/programs/firefox.nix
@@ -96,6 +96,7 @@ in
       sponsorblock.uuid = "sponsorBlocker@ajay.app";
       clearurls.uuid = "{74145f27-f039-47ce-a470-a662b129930a}";
       languagetool.uuid = "languagetool-webextension@languagetool.org";
+      new-tab-override.uuid = "newtaboverride@agenedia.com";
       karakeep = {
         uuid = "addon@karakeep.app";
         defaultArea = "navbar";
@@ -106,6 +107,7 @@ in
       enable = true;
 
       profiles.default = {
+        extraConfig = lib.readFile "${inputs.betterfox}/user.js";
         settings =
           let
             uiState = ''{"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":["sponsorblocker_ajay_app-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_74145f27-f039-47ce-a470-a662b129930a_-browser-action","ublock0_raymondhill_net-browser-action"],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","save-to-pocket-button","downloads-button","fxa-toolbar-menu-button","unified-extensions-button","sidebar-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["firefox-view-button","tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"]},"seen":["developer-button","sponsorblocker_ajay_app-browser-action","_762f9885-5a13-4abd-9c77-433dcd38b8fd_-browser-action","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","_74145f27-f039-47ce-a470-a662b129930a_-browser-action","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","unified-extensions-area","PersonalToolbar","TabsToolbar","toolbar-menubar"],"currentVersion":20,"newElementCount":5}'';
@@ -128,8 +130,6 @@ in
             "sidebar.revamp" = false;
             "media.eme.enabled" = true;
           };
-
-        extraConfig = lib.readFile "${inputs.betterfox}/user.js";
 
         search = lib.mkIf (cfg.searchEngine != null) {
           force = true;
