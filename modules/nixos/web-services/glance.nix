590937185bba38cd61f669bdb45177daa269efdc
SebastianStork <git@sstork.dev>
2026-03-12 18:44:23 +0100

    glance: Add bookmarks widget

--- modules/nixos/web-services/glance.nix
+++ modules/nixos/web-services/glance.nix
@@ -82,17 +82,134 @@ in
           name = "Home";
           center-vertically = true;
 
-          columns = lib.singleton {
-            size = "full";
-            widgets =
-              lib.singleton {
-                type = "search";
-                search-engine = "https://search.splitleaf.de/search?q={QUERY}";
-                autofocus = true;
-              }
-              ++ applicationSites
-              ++ observabilitySites;
-          };
+          columns = [
+            {
+              size = "full";
+              widgets =
+                lib.singleton {
+                  type = "search";
+                  search-engine = "https://search.splitleaf.de/search?q={QUERY}";
+                  autofocus = true;
+                }
+                ++ applicationSites
+                ++ observabilitySites;
+            }
+            {
+              size = "small";
+              widgets = lib.singleton {
+                type = "bookmarks";
+                groups = [
+                  {
+                    links = [
+                      {
+                        title = "YouTube";
+                        url = "https://www.youtube.com/";
+                      }
+                      {
+                        title = "DeepL";
+                        url = "https://www.deepl.com/en/translator";
+                      }
+                      {
+                        title = "GitHub";
+                        url = "https://github.com/SebastianStork";
+                      }
+                      {
+                        title = "ChatGBT";
+                        url = "https://chatgpt.com/";
+                      }
+                    ];
+                  }
+                  {
+                    title = "Email";
+                    links = [
+                      {
+                        title = "Mailbox";
+                        url = "https://app.mailbox.org/appsuite/#!!&app=io.ox/mail&folder=default0/INBOX";
+                      }
+                      {
+                        title = "Proton";
+                        url = "https://mail.proton.me/u/1/inbox";
+                      }
+                      {
+                        title = "h_da";
+                        url = "https://webmail.stud.h-da.de/stud/?_task=mail&_mbox=INBOX";
+                      }
+                    ];
+                  }
+                  {
+                    title = "Nix";
+                    color = "200 50 50";
+                    links = [
+                      {
+                        title = "Wiki";
+                        url = "https://wiki.nixos.org/wiki/Main_Page";
+                      }
+                      {
+                        title = "Packages Search";
+                        url = "https://search.nixos.org/packages";
+                      }
+                      {
+                        title = "NixOS Options Search";
+                        url = "https://search.nixos.org/options";
+                      }
+                      {
+                        title = "HM Options Search";
+                        url = "https://home-manager-options.extranix.com/";
+                      }
+                      {
+                        title = "Function Search";
+                        url = "https://home-manager-options.extranix.com/";
+                      }
+                      {
+                        title = "NixOS Manual";
+                        url = "https://nixos.org/manual/nixos/stable/";
+                      }
+                    ];
+                  }
+                  {
+                    title = "Infra";
+                    color = "140 70 50";
+                    links = [
+                      {
+                        title = "Backblaze";
+                        url = "https://secure.backblaze.com/b2_buckets.htm";
+                      }
+                      {
+                        title = "Healthchecks";
+                        url = "https://healthchecks.io/projects/ed5214d3-971f-4b66-997d-8ffd0d8cd4ca/checks/";
+                      }
+                      {
+                        title = "Hetzner";
+                        url = "https://console.hetzner.cloud/projects/10289618/servers";
+                      }
+                      {
+                        title = "Porkbun";
+                        url = "https://porkbun.com/";
+                      }
+                    ];
+                  }
+                  {
+                    title = "Uni";
+                    color = "10 70 50";
+                    links = [
+                      {
+                        title = "My";
+                        url = "https://my.h-da.de/";
+                      }
+                      {
+                        title = "Moodle";
+                        url = "https://lernen.h-da.de/";
+                      }
+                      {
+                        title = "GitLab";
+                        url = "https://code.fbi.h-da.de/";
+                      }
+                    ];
+                  }
+                ];
+              };
+            }
+          ];
         };
       };
     };
