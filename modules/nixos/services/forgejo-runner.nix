5f8b11e711892d8f45d2a285518a97630138f820
SebastianStork <git@sstork.dev>
2026-03-25 22:59:26 +0100

    forgejo-runner: Init module

---
+++ modules/nixos/services/forgejo-runner.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+  cfg = config.custom.services.forgejo-runner;
+in
+{
+  options.custom.services.forgejo-runner = {
+    enable = lib.mkEnableOption "";
+    forgejoUrl = lib.mkOption {
+      type = lib.types.nonEmptyStr;
+      default = "";
+    };
+    capacity = lib.mkOption {
+      type = lib.types.int;
+      default = 3;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    sops = {
+      secrets."forgejo-runner/token" = { };
+      templates."forgejo-runner.env".content = "TOKEN=${config.sops.placeholder."forgejo-runner/token"}";
+    };
+
+    services.gitea-actions-runner = {
+      package = pkgs.forgejo-runner;
+      instances.default = {
+        enable = true;
+        name = config.networking.hostName;
+        url = cfg.forgejoUrl;
+        tokenFile = config.sops.templates."forgejo-runner.env".path;
+        labels = [ "nixos:host" ];
+        settings.runner.capacity = cfg.capacity;
+      };
+    };
+  };
+}
