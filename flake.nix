662995ba4668b27a19afe4d076718540cbc16cbb
SebastianStork <git@sstork.dev>
2026-06-10 11:26:29 +0200

    flake: Update noctalia-shell input

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell/v4.7.5";
+      url = "github:noctalia-dev/noctalia-shell/v4.7.6";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
