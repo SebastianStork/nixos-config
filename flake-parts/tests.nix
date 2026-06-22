bf44c43f747c5f18e46bc91933575c873b176168
SebastianStork <git@sstork.dev>
2026-06-22 10:59:29 +0200

    tests: Build separately from flake checks

--- flake-parts/tests.nix
+++ flake-parts/tests.nix
@@ -1,10 +1,27 @@
-{ inputs, self, ... }:
 {
+  inputs,
+  self,
+  lib,
+  flake-parts-lib,
+  ...
+}:
+{
+  imports = lib.singleton (
+    flake-parts-lib.mkTransposedPerSystemModule {
+      name = "tests";
+      option = lib.mkOption {
+        type = lib.types.lazyAttrsOf lib.types.package;
+        default = { };
+      };
+      file = ./tests.nix;
+    }
+  );
+
   perSystem =
     { pkgs, ... }:
     let
       mkTest = name: {
-        name = "${name}-test";
+        inherit name;
         value = pkgs.testers.runNixOSTest {
           inherit name;
           _module.args = { inherit inputs self; };
@@ -13,6 +30,6 @@
       };
     in
     {
-      checks = "${self}/tests" |> self.lib.listDirectoryNames |> self.lib.genAttrs' mkTest;
+      tests = "${self}/tests" |> self.lib.listDirectoryNames |> self.lib.genAttrs' mkTest;
     };
 }
