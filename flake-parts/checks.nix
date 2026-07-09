0a71c2cebe373f4936a9dc67ed84fcba0c79dcc4
SebastianStork <git@sstork.dev>
2026-07-09 23:35:48 +0200

    checks: Add nebula certificate validation

--- flake-parts/checks.nix
+++ flake-parts/checks.nix
@@ -1,7 +1,12 @@
-{ inputs, self, ... }:
+{
+  inputs,
+  self,
+  lib,
+  ...
+}:
 {
   perSystem =
-    { pkgs, ... }:
+    { self', pkgs, ... }:
     {
       checks = {
         formatting = (inputs.treefmt.lib.evalModule pkgs "${self}/treefmt.nix").config.build.check self;
@@ -15,6 +20,35 @@
           deadnix --fail ${self}
           touch $out
         '';
+
+        nebula-certs =
+          let
+            nebulaHostsJson =
+              self.allHosts
+              |> lib.attrValues
+              |> lib.filter (host: host.config.custom.services.nebula.enable)
+              |> lib.map (
+                host:
+                let
+                  cfg = host.config.custom.services.nebula;
+                  netCfg = host.config.custom.networking;
+                in
+                {
+                  name = netCfg.hostName;
+                  certificate = toString cfg.certificateFile;
+                  publicKey = toString cfg.publicKeyFile;
+                  ca = toString cfg.caCertificateFile;
+                  networks = [ netCfg.overlay.cidr ];
+                  inherit (cfg) unsafeNetworks groups;
+                }
+              )
+              |> lib.toJSON
+              |> pkgs.writeText "nebula-hosts.json";
+          in
+          pkgs.runCommandLocal "nebula-certs" { } ''
+            ${lib.getExe self'.packages.nebula-check-certs} ${nebulaHostsJson}
+            touch $out
+          '';
       };
     };
 }
