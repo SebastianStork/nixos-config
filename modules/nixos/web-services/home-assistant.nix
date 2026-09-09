e503d9d26f3a7c9457427518b9c97c9d1f9a42d7
SebastianStork <git@sstork.dev>
2026-09-09 21:44:40 +0200

    home-assistant: Replace ZHA with Zigbee2MQTT

--- modules/nixos/web-services/home-assistant.nix
+++ modules/nixos/web-services/home-assistant.nix
@@ -1,6 +1,7 @@
 {
   config,
   inputs,
+  self,
   pkgs-unstable,
   lib,
   ...
@@ -8,6 +9,8 @@
 let
   cfg = config.custom.web-services.home-assistant;
   dataDir = config.services.home-assistant.configDir;
+  zigbee2mqttDataDir = config.services.zigbee2mqtt.dataDir;
+  mosquittoDataDir = config.services.mosquitto.dataDir;
 in
 {
   disabledModules = [ "services/home-automation/home-assistant.nix" ];
@@ -26,22 +29,77 @@ in
       default = 8123;
     };
     doBackups = lib.mkEnableOption "";
+    zigbee2mqtt = {
+      domain = lib.mkOption {
+        type = lib.types.nonEmptyStr;
+        default = "";
+      };
+      port = lib.mkOption {
+        type = lib.types.port;
+        default = 6836;
+      };
+      mqttPort = lib.mkOption {
+        type = lib.types.port;
+        default = 1883;
+      };
+      serialPort = lib.mkOption {
+        type = lib.types.nonEmptyStr;
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    services.home-assistant = {
-      enable = true;
-      package = pkgs-unstable.home-assistant;
-      extraComponents = [
-        "zha"
-        "google_translate"
-      ];
-      config = {
-        default_config = { };
-        automation = "!include automations.yaml";
-        script = "!include scripts.yaml";
-        scene = "!include scenes.yaml";
+    assertions = lib.singleton {
+      assertion = self.lib.isPrivateDomain cfg.zigbee2mqtt.domain;
+      message = self.lib.mkUnprotectedMessage "Zigbee2MQTT";
+    };
+
+    services = {
+      home-assistant = {
+        enable = true;
+        package = pkgs-unstable.home-assistant;
+        extraComponents = [
+          "google_translate"
+          "mqtt"
+        ];
+        config = {
+          default_config = { };
+          automation = "!include automations.yaml";
+          script = "!include scripts.yaml";
+          scene = "!include scenes.yaml";
+        };
       };
+
+      mosquitto = {
+        enable = true;
+        listeners = lib.singleton {
+          address = "127.0.0.1";
+          port = cfg.zigbee2mqtt.mqttPort;
+          omitPasswordAuth = true;
+        };
+      };
+
+      zigbee2mqtt = {
+        enable = true;
+        settings = {
+          homeassistant.enabled = true;
+          mqtt.server = "mqtt://127.0.0.1:${lib.toString cfg.zigbee2mqtt.mqttPort}";
+          serial = {
+            port = cfg.zigbee2mqtt.serialPort;
+            adapter = "ember";
+          };
+          frontend = {
+            enabled = true;
+            host = "127.0.0.1";
+            inherit (cfg.zigbee2mqtt) port;
+          };
+        };
+      };
+    };
+
+    systemd.services.zigbee2mqtt = {
+      after = [ "mosquitto.service" ];
+      requires = [ "mosquitto.service" ];
     };
 
     systemd.tmpfiles.rules = [
@@ -52,25 +110,44 @@ in
 
     custom = {
       services = {
-        caddy.virtualHosts.${cfg.domain} = {
-          inherit (cfg) port;
-          allowedGroups = [
-            "client"
-            "agent"
-          ];
+        caddy.virtualHosts = {
+          ${cfg.domain} = {
+            inherit (cfg) port;
+            allowedGroups = [
+              "client"
+              "agent"
+            ];
+          };
+          ${cfg.zigbee2mqtt.domain}.port = cfg.zigbee2mqtt.port;
         };
 
-        restic.backups.home-assistant = lib.mkIf cfg.doBackups {
-          conflictingService = "home-assistant.service";
-          paths = [ dataDir ];
+        restic.backups = {
+          home-assistant = lib.mkIf cfg.doBackups {
+            conflictingService = "home-assistant.service";
+            paths = [ dataDir ];
+          };
+          zigbee2mqtt = lib.mkIf cfg.doBackups {
+            conflictingService = "zigbee2mqtt.service";
+            paths = [ zigbee2mqttDataDir ];
+          };
         };
       };
 
-      persistence.directories = [ dataDir ];
+      persistence.directories = [
+        dataDir
+        zigbee2mqttDataDir
+        mosquittoDataDir
+      ];
 
-      meta.sites.${cfg.domain} = {
-        title = "Home Assistant";
-        icon = "sh:home-assistant";
+      meta.sites = {
+        ${cfg.domain} = {
+          title = "Home Assistant";
+          icon = "sh:home-assistant";
+        };
+        ${cfg.zigbee2mqtt.domain} = {
+          title = "Zigbee2MQTT";
+          icon = "sh:zigbee2mqtt";
+        };
       };
     };
   };
